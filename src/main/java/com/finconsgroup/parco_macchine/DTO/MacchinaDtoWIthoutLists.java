712c30282955b0da23914a4cdeaadbda5afc720e
Alessandro <alessandro.longo@finconsgroup.com>
2025-07-17 11:45:29 +0200

    modifiche

--- src/main/java/com/finconsgroup/parco_macchine/DTO/MacchinaDtoWIthoutLists.java
+++ src/main/java/com/finconsgroup/parco_macchine/DTO/MacchinaDtoWIthoutLists.java
@@ -1,2 +1,62 @@
-package com.finconsgroup.parco_macchine.DTO;public class MacchinaDtoWIthoutLists {
+package com.finconsgroup.parco_macchine.DTO;
+
+import com.finconsgroup.parco_macchine.enums.Stato;
+
+public class MacchinaDtoWIthoutLists {
+    private Long id;
+    private String nome;
+    private String descrizione;
+    private Stato stato;
+    private Integer tipologiaDto;
+
+    public MacchinaDtoWIthoutLists(Long id, String nome, String descrizione, Stato stato, Integer tipologiaDto) {
+        this.id = id;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.stato = stato;
+        this.tipologiaDto = tipologiaDto;
+    }
+
+    public MacchinaDtoWIthoutLists() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Stato getStato() {
+        return stato;
+    }
+
+    public void setStato(Stato stato) {
+        this.stato = stato;
+    }
+
+    public Integer getTipologiaDto() {
+        return tipologiaDto;
+    }
+
+    public void setTipologiaDto(Integer tipologiaDto) {
+        this.tipologiaDto = tipologiaDto;
+    }
 }
--- src/main/java/com/finconsgroup/parco_macchine/config/Configurat.java
+++ src/main/java/com/finconsgroup/parco_macchine/config/Configurat.java
@@ -1,2 +1,23 @@
-package com.finconsgroup.parco_macchine.config;public class Configurat {
+package com.finconsgroup.parco_macchine.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configurat {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true);
+
+        modelMapper.typeMap()
+    }
+
 }
